93e70f837fee5b7d7f85b1d4bcbcab15726d6be0
Bianca Tainá Lino Balduino <bianca.balduino@escola.pr.gov.br>
2025-10-23 12:17:12 +0000

    acabando

---
+++ script.js
@@ -0,0 +1,9 @@
+function acender(){
+    //document.getElementeById("lampada").src="on.png"
+    console.log("Luz acessa")
+}
+
+function apagar(){
+    //document.getElementeById("lampada").src="off.png"
+    console.log("Luz apagada")
+}
